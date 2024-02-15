72ba8573979f3f57ebfbcee69a74e6a21f5429ae
firaol19 <159700514+firaol19@users.noreply.github.com>
2024-02-15 09:50:56 +0300

    Add files via upload

---
+++ js.js
@@ -0,0 +1,20 @@
+function input() {
+    let inp = document.getElementById("form");
+    inp.style.display = "block";
+    inp.style.top = "70px"
+    
+}
+
+function delete1() {
+    let delete1 = document.getElementById("form");
+    delete1.style.display = "none"
+}
+function mobilewebe() {
+    let mobile = document.getElementById("mobileweb");
+    mobile.style.display = "block";
+    mobile.style.transform = "translatex(-185px)";
+}
+function delete2() {
+    let mob = document.getElementById("mobileweb");
+    mob.style.display = "none";
+}
